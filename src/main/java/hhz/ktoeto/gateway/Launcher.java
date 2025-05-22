26c0cb195bca7ed284e11caa85154d0c3bd222e1
Dmitriy Goncharov <ritm.dgoncharov@gmail.com>
2025-05-22 14:33:00 +0300

    CHORE: added dev configuration

--- src/main/java/hhz/ktoeto/gateway/Launcher.java
+++ src/main/java/hhz/ktoeto/gateway/Launcher.java
@@ -2,12 +2,32 @@ package hhz.ktoeto.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Launcher {
 
     public static void main(String[] args) {
         SpringApplication.run(Launcher.class, args);
+    }
+
+    @Bean
+    @Profile("dev")
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
     }
 
 }
